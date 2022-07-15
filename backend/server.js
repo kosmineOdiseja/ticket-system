d1b980cf8ec18e1c23aca88482ec2a361050d666
Alius <aliusbosas@gmail.com>
2022-07-15 14:43:41 +0300

    routes, controlers

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log("server... ")
+const { randomBytes } = require('crypto')
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+app.get('/', (req, res ) => {
+	res.status(200).json({message: "welcomme"})
+})
+// routes 
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.listen(PORT, () => console.log(`server started on Port ${PORT}`))
