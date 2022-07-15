37c80e1594f0f74e96ee2f9433e52b6bfd967ec5
Alius <aliusbosas@gmail.com>
2022-07-15 16:30:51 +0300

    commenst and validation for middleware

--- backend/controllers/userControler.js
+++ backend/controllers/userControler.js
@@ -1,8 +1,24 @@
-
+// @desc Register a new user
+// @route /api/users
+// @access Public
 const registerUser = (req, res) => {
+	// we can get undefine because we need to get bodyparser middleware.
+	// This is 
+	console.log(req.body)
+	// this how we get those variables 
+	const {name, email, password} = (req.body)
+
+	// validation
+	if(!name || !email || !password) {
+		res.status(400).json({message: "Please include all fields"})
+	}
+
 	res.send('Register Route')
 } 
 
+// @desc Login a new user
+// @route /api/users/login
+// @access Public
 const loginUser = (req, res) => {
 	res.send('Login Route')
 }
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
-const { randomBytes } = require('crypto')
 const express = require('express')
 const dotenv = require('dotenv').config()
-
-
 const PORT = process.env.PORT || 8000
 
 const app = express()
+
+// This is line for middleware parser
+ app.use(express.json())
+ app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res ) => {
 	res.status(200).json({message: "welcomme"})
