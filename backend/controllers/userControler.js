ec38bdbbc1ab0ce3a6f56b886f9a294cc1749895
Alius <aliusbosas@gmail.com>
2022-07-16 16:37:58 +0300

    login

--- backend/controllers/userControler.js
+++ backend/controllers/userControler.js
@@ -35,7 +35,7 @@ const registerUser = asyncHandler(async (req, res) => {
 		email,
 		password: hashedPassword
 	})
-	
+
 
 	if (user) {
 		res.status(201).json({
@@ -53,8 +53,24 @@ const registerUser = asyncHandler(async (req, res) => {
 // @desc Login a new user
 // @route /api/users/login
 // @access Public
-const loginUser = asyncHandler(async (req, res) => {
-	res.send('Login Route')
+const loginUser = asyncHandler(async(req, res) => {
+
+	const { email, password } = req.body
+
+	const user = await User.findOne({email})
+	// console.log(user, 'this is user ')
+	
+// check user and password match.
+	if( user && (await bcrypt.compare(password, user.password))) {
+		res.status(200).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email
+		})
+	} else {
+		res.status(401) 
+		throw new Error('Invalid credentials')
+	}
 })
 
 module.exports = {
