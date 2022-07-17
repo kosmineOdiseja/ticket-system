501dc86595655d52c9c05986f79c2ccc97e2607e
Alius <aliusbosas@gmail.com>
2022-07-17 22:30:29 +0300

    react routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
-    <div className="App">
-        my app 
-    </div>
+    <>
+      <Router>
+       <div className='container'>
+          <Routes> 
+            <Route path='/' element={<Home />}/>
+            <Route path='/login' element={<Login />}/>
+            <Route path='/register' element={<Register/>}/>
+          </Routes>
+        </div> 
+      </Router>
+    </>
   );
 }
 
