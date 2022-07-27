fdb610aae9fbe003bd7770184a210893b3fcfa99
Alius <aliusbosas@gmail.com>
2022-07-27 15:55:12 +0300

    display single ticket

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -7,7 +7,7 @@ import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 
 function Ticket() {
-  const { ticket, isLoader, isSuccess, isError, message } = useSelector(
+  const { ticket, isLoading, isSuccess, isError, message } = useSelector(
     (state) => state.tickets
   );
   const params = useParams();
@@ -19,9 +19,39 @@ function Ticket() {
       toast.error(message);
     }
     dispatch(getTicket(ticketId));
+    // eslint-disabled-next-line
   }, [isError, message, ticketId]);
 
-  return <div>Ticket</div>;
+  if (isLoading) {
+    return <Spinner />;
+  }
+  if (isError) {
+    return <h3> Something went wrong</h3>;
+  }
+
+  return (
+    <div>
+      <header className="ticket-header">
+        <BackButton url="/tickets" />
+        <h2>
+          Ticket ID: {ticket._id}
+          <span className={`status status-${ticket.status}`}>
+            {ticket.status}
+          </span>
+        </h2>
+        <h3>
+          Date Submitted: {new Date(ticket.createdAt).toLocaleString("lt-LT")}
+        </h3>
+        <h3>Product: {ticket.product}</h3>
+        <hr />
+        <div className="ticket-desc">
+          <h3>Description of Issue</h3>
+          <p>{ticket.description}</p>
+        </div>
+        <h2>Notes</h2>
+      </header>
+    </div>
+  );
 }
 
 export default Ticket;
